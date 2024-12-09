231a0cf54f1b1539eb1d779fa6f675e6afd723a0
augustodomingos28 <augusto.domingos@unimedsc.coop.br>
2024-12-09 09:47:42 -0300

    starting to create templates

--- pyrails/templates/settings_template.py
+++ pyrails/templates/settings_template.py
@@ -1,13 +1,21 @@
-from pydantic_settings import BaseSettings
+settings_template = """from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = 'sqlite:///db.sqlite3'
+    model_config = SettingsConfigDict(
+        env_file='.env', env_file_encoding='utf-8', extra='ignore'
+    )
+
+    DATABASE_URL: str
 
     APP_NAME: str
     APP_VERSION: str
     APP_DESCRIPTION: str
     APP_SLUG: str
 
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
 settings = Settings()
+"""
